f12702c863caae99b5c92ccf7e2efca86d40af1b
Mengyuuuuuuu <guiqulaixi2021@gmail.com>
2024-06-12 18:48:57 +0200

    update1

--- lib/home.dart
+++ lib/home.dart
@@ -11,7 +11,7 @@ class _homeState extends State<home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.red,
       
     );
   }
